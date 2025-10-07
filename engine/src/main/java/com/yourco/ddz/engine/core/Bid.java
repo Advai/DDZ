977c472ba5744b1e6209b8ca68e658f86a91c742
Rishab Nathan <rishabnathan@Mac.ht.home>
2025-10-06 22:02:49 -0500

    starting proper bid loop logic

---
+++ engine/src/main/java/com/yourco/ddz/engine/core/Bid.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+package com.yourco.ddz.engine.core;
+
+/**
+ * @author rishabnathan
+ */
+public class Bid {
+  public Bid(int value) {
+    this.value = value;
+  }
+
+  private final int value;
+
+  public int getValue() {
+    return value;
+  }
+}
